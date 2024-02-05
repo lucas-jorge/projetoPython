d270ddc58b4d87f769701ad3cb52f651812de31d
lucas-jorge <lucasajorge@gmail.com>
2024-02-05 17:19:11 -0300

    updating

--- app.py
+++ app.py
@@ -1,46 +1,50 @@
 import os
 
+# Lista de dicionários contendo informações sobre os restaurantes
 restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
                 {'nome':'Pizza Suprema', 'categoria':'Pizza', 'ativo':True},
                 {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False}]
 
+# Função para exibir o nome do programa
 def exibir_nome_do_programa():
     print("""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
-""")
+    # Arte ASCII representando o nome do programa
+    """)
 
+# Função para exibir as opções do menu
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
     print('3. Alternar estado do restaurante')
     print('4. Sair\n')
 
+# Função para finalizar o aplicativo
 def finalizar_app():
     exibir_subtitulo('Finalizar app')
 
+# Função para retornar ao menu principal
 def voltar_ao_menu_principal():
     input('\nDigite uma tecla para voltar ao menu ')
     main()
 
+# Função para tratar opção inválida
 def opcao_invalida():
     print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
+# Função para exibir um subtítulo
 def exibir_subtitulo(texto):
-    os.system('cls')
+    os.system('cls')  # Limpa a tela
     linha = '*' * (len(texto))
     print(linha)
     print(texto)
     print(linha)
     print()
 
+# Função para cadastrar um novo restaurante
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
+    # Solicita dados do restaurante
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
     dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
@@ -49,10 +53,11 @@ def cadastrar_novo_restaurante():
     
     voltar_ao_menu_principal()
 
+# Função para listar os restaurantes
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
-
-    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
+    # Exibe os restaurantes formatados
+    print(f'{"Nome do restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
@@ -61,8 +66,9 @@ def listar_restaurantes():
 
     voltar_ao_menu_principal()
 
+# Função para alternar o estado de ativação de um restaurante
 def alternar_estado_restaurante():
-    exibir_subtitulo('ALterando estado do restaurante')
+    exibir_subtitulo('Alterando estado do restaurante')
     nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
     restaurante_encontrado = False
 
@@ -78,10 +84,10 @@ def alternar_estado_restaurante():
             
     voltar_ao_menu_principal()
 
+# Função para escolher uma opção do menu
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
-        # opcao_escolhida = int(opcao_escolhida)
 
         if opcao_escolhida == 1: 
             cadastrar_novo_restaurante()
@@ -96,11 +102,13 @@ def escolher_opcao():
     except:
         opcao_invalida()
 
+# Função principal que inicia o programa
 def main():
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
+# Verifica se o script é o ponto de entrada principal
 if __name__ == '__main__':
     main()
